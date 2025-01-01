61d8cba5e72b3f0adba8a4ebd764b325681d445e
Animiiexe <animesh2325@gmail.com>
2025-01-01 16:51:08 +0530

    module fix

--- src/main.js
+++ src/main.js
@@ -1,68 +1,65 @@
-console.log("Connected");
-
-const carouselContainerEl = document.querySelector(".carousel-inner");
 const selectEl = document.querySelector("select");
+const carouselContainerEl = document.querySelector(".carousel-inner");
 
-console.log(carouselContainerEl, selectEl);
+// console.log(selectEl, carouselContainerEl);
 
-const base_url = "https://dog.ceo/api/";
+const BASE_URL = "https://dog.ceo/api/";
 
 
-// gets list of dogs
-async function getDogslist() {
-  try {
-    const response = await fetch(`${base_url}breeds/list/all`);
-    const data = await response.json();
-    return Object.keys(data.message);
-  } catch (error) {
-    console.log(error);
-  }
+// === MARK: Fetch
+// Gets the list of dog breeds from api
+async function getDogsList() {
+    try {
+        const response = await fetch(`${BASE_URL}breeds/list/all`);
+        const data = await response.json();
+        return Object.keys(data.message);
+    } catch (err) {
+        console.log(err);
+    }
+}
+getDogsList();
+
+// Gets list of 10 images on breed
+async function getDogImages(breed) {
+    try {
+        const res = await fetch(`${BASE_URL}breed/${breed}/images`);
+        const data = await res.json();
+        return data.message.slice(0, 10);
+    } catch (err) {
+        console.log(err);
+    }
 }
 
-getDogslist();
 
-
-// gets list of 10 images on breed
-async function getDogsimages(breed) {
-  try {
-    const res = await fetch(`${base_url}breed/${breed}/images`);
-    const data = await res.json();
-    return data.message.slice(0, 10);
-  } catch (error) {
-    console.log(error);
-  }
-}
-getDogsimages("borzoi")
-
+//MARK: Render
 async function renderOptions(){
-   const breedList = await getDogslist();
-   for (breed of breedList){
-    const option = document.createElement("option")
-    option.textContent = breed[0].toUpperCase() + breed.slice(1).toLowerCase();
-    option.value = breed;
+    const breedList = await getDogsList();
 
-    selectEl.appendChild(option)
-   }
+    for( let breed of breedList){
+        const option = document.createElement("option");
+        option.textContent = breed[0].toUpperCase() + breed.slice(1).toLowerCase();
+        option.value = breed;
+        selectEl.appendChild(option);
+    }
 }
- renderOptions()
+renderOptions();
 
-
-async function renderCarousel(breed){
-  carouselContainerEl.innerHTML =''
-  const images = await getDogsimages(breed);
- 
-  for (let i = 0; i < images.length; i++) {
-    const div = document.createElement("div");
-    div.classList.add("carousel-item", i === 0 && "active");
-    div.innerHTML = `<img
-              src="${images[i]}"
-              class="d-block w-100 rounded-3"
-              alt="${breed}"
-            />`;
-    carouselContainerEl.appendChild(div);
- }
-
+async function renderCarousel(breed) {
+    carouselContainerEl.innerHTML = ''
+    const images = await getDogImages(breed);
+    
+    for (let i = 0; i < images.length; i++){
+        const div  = document.createElement("div");
+        div.classList.add("carousel-item", i===0 && "active");
+        div.innerHTML = `<img src="${images[i]}" 
+          class="d-block w-100 rounded-3 "
+          alt="${breed}">`;
+        carouselContainerEl.appendChild(div);
+    }
 }
 
+selectEl.addEventListener("change", function (e) {
+    renderCarousel(e.target.value);
+});
 
-selectEl.addEventListener("change", function(e){renderCarousel(e.target.value)})
+renderCarousel("borzoi");
